b76f97acab9850bb434c4a9deaf7b97e0d84e5df
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-10 23:36:27 -0400

    created visualization plot

---
+++ lung-cancer/visualize.py
@@ -0,0 +1,78 @@
+import csv                          # view data
+import matplotlib.pyplot as plt     # plotting resutls
+import numpy as np
+import pandas as pd
+
+# for cleaning up data and adding newlines when text is too long
+def add_newlines(str):
+    return str.replace(" ", "\n", 1)
+
+# columns to use as reference when doing the analysis: 
+# these are possible symtoms one might face if they have lung cancer
+SYMPTOMS = ["YELLOW_FINGERS", "COUGHING", "SHORTNESS OF BREATH", "SWALLOWING DIFFICULTY", "CHEST PAIN", "WHEEZING", "FATIGUE", "YELLOW_FINGERS"]
+# these are factors which may or may not lead to lung cancer
+CAUSES = ["SMOKING", "ALCOHOL CONSUMING", "ANXIETY"]
+
+# label for graph, depending on what is being analyzed
+X_LABEL = "Symptoms and Causes"    
+
+# obtain measurements
+df = pd.read_csv("lung-cancer/data/lung-cancer.csv", usecols=SYMPTOMS+CAUSES)
+cancer_results = pd.read_csv("lung-cancer/data/lung-cancer.csv", usecols=["LUNG_CANCER"])["LUNG_CANCER"]
+
+# determines bar heights
+yes_symptom_yes_cancer = []
+no_symptom_yes_cancer  = []
+for stat in df:
+
+    # stores counts of people
+    total_count = yes_count = no_count = 0
+
+    # stores results and goes through each one
+    results = df[stat]
+    for person in range(len(results)):
+    
+        # checks if they said no to having cancer
+        if cancer_results[person] == "NO":    
+            continue
+
+        # appends counts accordingly
+        if results[person] == 2:
+            yes_count += 1
+        else:
+            no_count += 1
+        total_count += 1
+
+    # determines percents
+    yes_symptom_yes_cancer.append(yes_count / total_count * 100)
+    no_symptom_yes_cancer.append(no_count / total_count * 100)
+
+# creates arrays of 100 percents
+yes_symptom_no_cancer = [100 for _ in range(len(yes_symptom_yes_cancer))]
+no_symptom_no_cancer = [100 for _ in range(len(no_symptom_yes_cancer))]
+
+# settings for plot
+stat_count = len(yes_symptom_yes_cancer)
+x_size, y_size = 10, 5
+fig = plt.figure(figsize=(x_size, y_size))
+bar_width = x_size / stat_count / 3
+
+# determines x-axis
+x_axis = np.arange(1, stat_count+1, 1)
+
+# plots yes_cancer bar
+plt.bar(x_axis, yes_symptom_no_cancer, bar_width, color="lightblue", label="Symptom, No Cancer")
+plt.bar(x_axis, yes_symptom_yes_cancer, bar_width, color="royalblue", label="Symptom, Cancer")
+
+plt.bar(x_axis + bar_width * 1.1, no_symptom_no_cancer, bar_width, color="pink", label="No Symptom, No Cancer")
+plt.bar(x_axis + bar_width * 1.1, no_symptom_yes_cancer, bar_width, color="red", label="No Symptom, Cancer")
+
+# sets labels
+plt.xticks(x_axis + bar_width / 2, map(add_newlines, df.keys()), fontsize=5)
+plt.xlabel(X_LABEL)
+plt.ylabel("Percent of Reponses")
+plt.title("")
+
+# shows figure
+plt.legend(loc="best")
+plt.show()
